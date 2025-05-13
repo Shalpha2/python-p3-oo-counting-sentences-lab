bcc63ce956937454167add8976b59f40eb933815
Shadrack Kiprono <shadrackkiprono00@gmail.com>
2025-05-13 14:57:20 -0700

    refactor

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,33 @@
-#!/usr/bin/env python3
+
+import re
 
 class MyString:
-  pass
+    def __init__(self, value=''):
+        self._value = ''
+        self.value = value  # Use the setter for initial assignment
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, val):
+        if isinstance(val, str):
+            self._value = val
+        else:
+            print("The value must be a string.")
+
+
+    def is_sentence(self):
+        return self.value.endswith('.')
+
+    def is_question(self):
+        return self.value.endswith('?')
+
+    def is_exclamation(self):
+        return self.value.endswith('!')
+
+    def count_sentences(self):
+        split_sentences = re.split(r'[.!?]+', self.value)
+        sentences = [s for s in split_sentences if s.strip()]
+        return len(sentences)
